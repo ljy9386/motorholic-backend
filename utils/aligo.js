c000882625967e880159de76edd7a316a974b6d5
ljy9386 <ljy9386@naver.com>
2025-08-03 01:52:41 +0900

    Add files via upload

--- utils/aligo.js
+++ utils/aligo.js
@@ -1,30 +1,4 @@
 // utils/aligo.js
-const axios = require('axios');
-require('dotenv').config();
-
-async function sendSMS({ receiver, msg }) {
-  try {
-    const res = await axios.post('https://apis.aligo.in/send/', null, {
-      params: {
-        key: process.env.ALIGO_API_KEY,
-        user_id: process.env.ALIGO_USER_ID,
-        sender: process.env.ALIGO_SENDER,
-        receiver, // 받는사람 번호
-        msg,      // 문자 내용
-        testmode_yn: 'N' // 실제 전송: N, 테스트: Y
-      }
-    });
-
-    console.log('📤 문자 전송 결과:', res.data);
-    return res.data;
-  } catch (err) {
-    console.error('❌ 문자 전송 실패:', err.response?.data || err.message);
-    throw err;
-  }
-}
-
-module.exports = sendSMS;
-// utils/aligo.js
 require('dotenv').config();
 const axios = require('axios');
 const qs = require('qs');
@@ -35,9 +9,9 @@ async function sendSMS({ receiver, msg }) {
     key: process.env.ALIGO_API_KEY,
     user_id: process.env.ALIGO_USER_ID,
     sender: process.env.ALIGO_SENDER,
-    receiver, // 받는사람 번호
-    msg,      // 문자 내용
-    testmode_yn: 'N' // 실제 전송: N, 테스트: Y
+    receiver,
+    msg,
+    testmode_yn: 'N'
   };
 
   try {
